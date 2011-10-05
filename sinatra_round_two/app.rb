91ade25bf621cfde65ddee03b1be42d28089142a
Steven Klise <steven@stevenklise.com>
2011-10-05 17:33:01 -0400

    Getting ready for class

--- sinatra_round_two/app.rb
+++ sinatra_round_two/app.rb
@@ -33,10 +33,8 @@ get '/' do
   # Sinatra, not to a specific file so we can use them between the controller
   # and the view.
   @time = Time.now
-  # Instead of just putting the time on the page, let's use the attributes
-  # of the time (minutes, hours, date) to create a colored div.
   # Also, let's give our site a Title
-  @title = "ITP Web Dev Presents Colored Boxes"
+  @title = "ITP Web Dev Blog"
   
   # Here we call our template with the method `erb`. Erb is a templating
   # system for Ruby. Templates allow us to have only a few pages and fill
@@ -100,4 +98,90 @@ end
 # every time we call `erb`. So if you open up `main.erb` you'll notice there
 # are none of the HTML `head` elements, just the content. Awesome.
 
-# 
+# Look at `main.erb`, it's three lines long, one of them a long line. What I'm
+# doing there is taking the `@time` variable and calling a few methods on
+# it. Refer to the [Ruby-doc for Time](http://www.ruby-doc.org/core/Time.html) for
+# the methods I called, and play around with your own to reformat the date.
+
+# Alright, back to our application. It just shows the time, let's make it in 
+# to a simple blog because everyone loves blogging.
+
+# We'll make a controller called **article** for our blog article
+get '/article' do
+  # Using Ruby's Hash class to encapsulate the Title and Content in
+  # one instance variable.
+  @article = {"title" => "My first blog post", "content" => "Hello nerds, I'm starting a blog."}
+  # Render the newly created `article.erb` page.
+  erb :article
+end
+
+# How would I make a second article?
+# Re-do everything from `/article`, this time at `/article/two`
+get '/article/two' do
+  @article = {"title" => "Really getting going with this whole blogging thing.",
+    "content" => "Look at me right now, totally blogging up a storm. Don't you dare try to stop me."}
+
+  erb :article
+end
+
+# This is going to get ugly, but the way to fix this is with databases and
+# we aren't there yet. First we need an interface to add new articles.
+# The start of the interface is a controller to get there
+get '/article/new' do
+  # Then we need a view with a form
+  erb :new_article
+end
+
+# Let's look at the file `new_article.erb` in depth.
+#
+#       <article>
+#       <h1>Write a new Article</h1>
+#       
+#       <form action="/article/create" method="post">
+#         <div>
+#           <label for="title">Title:</label>
+#           <input name="title" type="text"/>
+#         </div>
+#         <div>
+#           <label for="content">Content:</label>
+#           <textarea name="content"></textarea>
+#         </div>
+#         <input type="submit"/>
+#       </form>
+#      
+#       <p><a href="/">Home</a></p>
+#       </article>
+
+
+# Actually, we can skip right
+# to the `form` tag. It's got two attributes:
+#
+#   * **action** is the url which will be requested when the form is submitted
+#   * **method** here we choose the verb we want to make the request with.
+#     It can be `get`, `post`, `put` or `delete`. We are using `post` because
+#     we are creating something.
+#
+# Then we have `label` tags which when combined with the `for=` attribute are
+# rather self explanatory. `input` and `textarea` are two different form fields
+# with their own syntaxes, `textarea` requires a close tag but `input` does not.
+# They both need a `name=` attribute, the value of this attribute will tell
+# the server what the name of the inputs is.
+
+# When you hit submit, you'll get sent to `/article/create` and it will be a _POST_
+# request. Let's make that controller.
+post '/article/create' do
+  # The parameters sent with this HTTP request are saved by Sinatra in the `params` hash.
+  # A handy trick to see what are in your params is to raise an error to see the params dump.
+  #
+  # raise params.inspect
+  #
+  @article = params
+  # Ahh, the beauty of templates, reusing the same erb file.
+  erb :article
+end
+
+# You'll notice that this data does not persist. If you try to go back to `/article/create`
+# nothing is there. It probably will give you an error. Since this was
+# a _POST_ request and not a _GET_. We need databases!
+
+# Stay tuned.
